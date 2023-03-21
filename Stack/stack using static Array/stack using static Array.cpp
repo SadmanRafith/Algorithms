9db5de157a0c989d1be51333dd91f1a91a0a4305
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-03-22 00:07:58 +0600

    Add files via upload

---
+++ Stack/stack using static Array/stack using static Array.cpp
@@ -0,0 +1,55 @@
+/*
+----->using static array
+-------> push/pop/isEmpty/isFull
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+# define n 3
+
+int a[n];
+int top=-1;
+
+int isEmpty(){
+    return top==-1?1:0;
+}
+int isFull(){
+    return top>=n-1?1:0;
+}
+
+void push(int value){
+    if(!isFull()) a[++top]=value;
+    else cout<<"Full"<<endl;
+}
+void pop(){
+    if(!isEmpty()) top--;
+    else cout<<"Empty"<<endl;
+}
+
+int peak(){
+    return a[top];
+}
+
+void display(){
+    for(int i=0;i<=top;i++){
+        cout<<a[i]<<" ";
+    }
+
+}
+
+int main(){
+
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    display();
+    pop();
+    pop();
+    pop();
+    pop();
+
+return 0;
+}
