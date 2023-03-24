4404c74d21885233e877955af84f54c686c56f38
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-03-24 22:55:42 +0600

    Add files via upload

---
+++ Stack/Stack stl/Stack STL.cpp
@@ -0,0 +1,24 @@
+/*
+----->using static array
+-------> push/pop/isEmpty/isFull
+*/
+
+
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+
+
+
+int main(){
+    stack<string> s();
+    s.push("Richard");
+    s.push("Aaron");
+    s.push("Florence");
+    s.push("Jonathan");
+    s.display();
+    cout<<s.top()<<endl;
+
+return 0;
+}
+
