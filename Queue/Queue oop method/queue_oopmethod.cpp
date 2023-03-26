fec5f4fe40c3524ddadac4843c595898e230f0ef
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-03-26 22:40:55 +0600

    Add files via upload

---
+++ Queue/Queue oop method/queue_oopmethod.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class Queue {
+private:
+    int* arr;
+    int front, rear, size;
+public:
+    Queue(int n) {
+        size = n;
+        arr = new int[size];
+        front = -1;
+        rear = -1;
+    }
+
+    bool isEmpty() {
+        return (front == -1);
+    }
+
+    bool isFull() {
+        return (rear == size - 1);
+    }
+
+    void enqueue(int x) {
+        cout << "Enqueuing " << x << endl;
+        if (isFull()) {
+            cout << "Error: Queue is full" << endl;
+            return;
+        }
+        if (isEmpty()) {
+            front = 0;
+        }
+        rear++;
+        arr[rear] = x;
+    }
+
+    void dequeue() {
+        if (isEmpty()) {
+            cout << "Error: Queue is empty" << endl;
+            return;
+        }
+        cout << "Dequeuing " << arr[front] << endl;
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        }
+        else {
+            front++;
+        }
+    }
+
+    ~Queue() {
+        delete[] arr;
+    }
+};
+
+int main() {
+    Queue q(3);
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+    return 0;
+}
